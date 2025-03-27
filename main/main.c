faca4c79d5c31cdff90c215edfbcace0680cab82
CataphractScript <dowlatabadiparsa@gmail.com>
2025-03-28 00:16:33 +0330

    Operations
    Operations of the main menu options

--- main/main.c
+++ main/main.c
@@ -54,7 +54,7 @@ int main()
         puts("See you later...");
 
         return 0;
-      //   break;
+        // break;
 
       default:
         chosen_option_menu = ZERO;
@@ -65,8 +65,118 @@ int main()
         break;
       }
 
-    }while (command.cM != 6);
 
-    //To be continued... 
+      switch (chosen_option_menu) // get new menu number
+      {
+      case ADDS:
+        info addI; // temp
+        grades addG; // temp
+
+        GetStudentInfo(&addI, &addG);
+
+        sfptr = fopen("../files/student.dat", "rb+");
+
+        if (sfptr == NULL)
+        {
+          fputs("Can not open the student file !!!", stderr);
+        }
+
+        fseek(sfptr, sizeof(info) * (addI.student_id - 1), SEEK_SET);
+        fwrite(&addI, sizeof(info), 1, sfptr);
+        fclose(sfptr);
+
+        // copy grades from student.dat to grades.dat
+
+        gsfptr = fopen("../files/grades.dat", "rb+");
+
+        if (sfptr == NULL)
+        {
+          fputs("Can not open the grades file !!!", stderr);
+        }
+
+        fseek(gsfptr, sizeof(grades) * (addG.student_id - 1), SEEK_SET);
+        fwrite(&addG, sizeof(grades), 1, gsfptr);
+        fclose(gsfptr);
+
+        printf("\n");
+
+        chosen_option_CSI = ZERO;
+
+        break;
+
+      case ADDSG:
+
+        grades add_grades;
+        temp add_ID;
+
+        gsfptr = fopen("../files/grades.dat", "rb+");
+
+        if (gsfptr == NULL)
+        {
+          fputs("Can not open the grades file !!!", stderr);
+        }
+
+        printf("\n");
+        printf("Enter student id : ");
+        scanf("%u", &add_ID.enter_id);
+
+        fseek(gsfptr, sizeof(grades) * (add_ID.enter_id - 1), SEEK_SET);
+        fread(&add_grades, sizeof(grades), 1, gsfptr);
+
+        AddStudentGrades(&add_grades);
+
+        fseek(gsfptr, sizeof(grades) * (add_ID.enter_id - 1), SEEK_SET);
+        fwrite(&add_grades, sizeof(grades), 1, gsfptr);
+
+        fclose(gsfptr);
+
+        break;
+     
+      case RS:
+
+        temp SID; // SID : Student ID
+
+        sfptr = fopen("../files/student.dat", "rb+");
+        gsfptr = fopen("../files/grades.dat", "rb+");
+
+        if (sfptr == NULL)
+        {
+          fputs("Can not open the student file !!!", stderr);
+          return 0;
+        }
+
+        if (gsfptr == NULL)
+        {
+          fputs("Can not open the grades file !!!", stderr);
+          return 0;
+        }
+
+        printf("\n");
+        printf("Enter student id : ");
+        scanf("%u", &SID.enter_id);
+          
+        RemoveStudent(SID.enter_id, sfptr, gsfptr);
+
+        fclose(sfptr);
+        fclose(gsfptr);
+
+        printf("\n");
+
+        chosen_option_CSI = ZERO;
+        
+        break;
+
+      case CSI:
+        command.c1 = getcommand1();
+        break;
+
+      case PSI:
+        command.c2 = getcommand2();
+        break;
+      }
+
+      //To be continued...
+    }while (command.cM != 6);
+    
     return 0;
 }
